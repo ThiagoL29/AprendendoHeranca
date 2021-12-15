8279980f94546c6420d244bf391c1a2be8a13045
ThiagoL29 <93353694+ThiagoL29@users.noreply.github.com>
2021-12-14 23:40:52 -0300

    Update pessoa.java

--- pessoa.java
+++ pessoa.java
@@ -11,23 +11,26 @@ public class pessoa {
         nome = n;
         sexo = s;
     }
+    
     public void mostrarPessoa() {
 
         System.out.print("Idade: " + idade + "\n" + "Nome: " +nome +"\n" + "Sexo: " + sexo +"\n\n\n");
     }
+    
     public void fazerAniversario(){
         this.idade++;
     }
 
     public void mudancaDeNome(String n){
         this.nome = n;
-
     }
 
     public void novaIdade(int i){
         idade = i;
     }
+    
     public void mudancaSexo(char s){
-        sexo = s;}
+        sexo = s;
+    }
 }
 
